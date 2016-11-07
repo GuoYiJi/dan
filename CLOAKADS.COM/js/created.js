a929f21f59f04db40df40ed3a4195436a8527aa1
Shuai Guo <735183844@qq.com>
2016-11-07 15:22:07 +0800

    fix

--- CLOAKADS.COM/js/created.js
+++ CLOAKADS.COM/js/created.js
@@ -1,4 +1,21 @@
+function initPos(parent){
+  var pos = []
+  // 初始化定位在input里面的tag
+  parent.find('.J-inputDefault').css( 'min-height', parent.find('.J-inputDefault').height() || 42 )
+  parent.find('.J-deleteTag:visible').each(function (){
+    pos.push($(this).position())
+  }).each(function (i){
+    $(this).css({
+      position: 'absolute',
+      zIndex: 11,
+      left: pos[i].left,
+      top: pos[i].top,
+    })
+  })
+  return pos
+}
 $(function (){
+  initPos($('#J-inputDefault1'))
 
   // Enable or Disable
   $('.J-checkRadio').on('click', function (){
@@ -8,10 +25,12 @@ $(function (){
 
   // menu
   $('.J-selectToggle').on('click', function (){
+    var pos
     var $this = $(this), $showContent = $this.next()
     if ($this.data('show') === 'hide') {
       $this.data('show', 'show').find('i').text('-')
       $showContent.show()
+      pos = initPos($showContent)
     } else {
       $this.data('show', 'hide').find('i').text('+')
       $showContent.hide()
@@ -24,7 +43,7 @@ $(function (){
   })
 
   // menu-select__bd
-  $('.J-menuSelectSub input').focus(function (){
+  $('.J-inputDefault input').focus(function (){
     var menuHeight = 0, $menu, $this= $(this), $menuSub
     $menu = $this.parents('.J-menu')
     $menuSub = $this.parent().next()
@@ -32,7 +51,51 @@ $(function (){
     $menu.height(menuHeight)
     $menuSub.show()
   }).blur(function (){
-    $(this).parent().next().hide()
-    $('.J-menu').height('auto')
+    var _this = $(this)
+    setTimeout(function (){
+      _this.parent().next().hide()
+      $('.J-menu').height('auto')
+    }, 200)
+    _this.parent().find('.J-deleteTag').css({
+      position: 'relative',
+      left: 'auto',
+      top: 'auto'
+    })
+  })
+
+  $('.J-menuSelectSub a').on('click', function (){
+    var _html = '<div class="create-form__tag J-deleteTag"><i>x</i>' + $(this).text() + '</div>', $last
+
+    $last = $(this).parent().parent().prev().append(_html).find('.J-deleteTag').last()
+    $last.css({
+      position: 'absolute',
+      left: $last.position().left,
+      top: $last.position().top,
+      zIndex: 11
+    })
+  })
+
+
+  // deleteTag
+  $('.J-inputDefault').on('click', '.J-deleteTag', function (){
+    var pos = [], $deleteTag
+    $deleteTag = $(this).parent().find('.J-deleteTag')
+    $deleteTag.css({
+      position: 'relative',
+      left: 'auto',
+      top: 'auto'
+    })
+    $(this).remove()
+
+    $deleteTag.each(function (){
+      pos.push($(this).position())
+    }).each(function (i){
+      $(this).css({
+        position: 'absolute',
+        zIndex: 11,
+        left: pos[i].left,
+        top: pos[i].top,
+      })
+    })
   })
 })
